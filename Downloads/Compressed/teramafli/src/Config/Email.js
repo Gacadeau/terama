2408657f19c84dc9e378e677e98a73057649bfb6
Gacadeau <gashimwea@gmail.com>
2024-02-04 10:48:38 +0200

    first commit

--- Downloads/Compressed/teramafli/src/Config/Email.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-export const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'teramaflix@gmail.com',
-      pass: 'cbglwxpkcfeekrwj'
-    }
-  });
