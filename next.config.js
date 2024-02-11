2b63c305ba9c8c3ea1e3645036c116197305a532
Gacadeau <gashimwea@gmail.com>
2024-02-11 12:02:05 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost,terama.vercel'],
+    domains: ['localhost', 'terama.vercel'],
   },
 };
 
--- src/components/Cacheds/Cached.js
+++ src/components/Cacheds/Cached.js
@@ -35,7 +35,6 @@ function Cached({ video }) {
     fetchProfile(video.Photo)
     fetchImage()
   }, [video])
-
   return (
     <>
       <div className="video1 flex flex-row w-full  justify-between md:px-6 mb-6 cursor-pointer ">
@@ -44,7 +43,8 @@ function Cached({ video }) {
 
             {
                 <Link href={`/Watch?v=${video.uniid}`}>
-                  <Image width={100} height={100} src={imageBlobUrl} className="w-full h-full object-cover" alt="profil" />
+                   
+                  <Image width={100} height={100} src={`${process.env.NEXT_PUBLIC_URL}/Thumbnails/${video.Image}`} className="w-full h-full object-cover" alt="profil" />
                 </Link>
             }
 
